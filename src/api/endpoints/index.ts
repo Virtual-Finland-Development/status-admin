dd08cf98235bdb7ead67ed61f23346dba0c50a47
Lauri Saarela <lauri.saarela@gofore.com>
2023-01-25 10:10:03 +0200

    remove trailing slash from base url

--- src/api/endpoints/index.ts
+++ src/api/endpoints/index.ts
@@ -1,2 +1,8 @@
-export const STATUS_INFO_API_BASE_URL =
-  import.meta.env.VITE_STATUS_INFO_API_BASE_URL || 'http://0.0.0.0:5747';
+// utils
+import { removeTrailingSlash } from '../../utils';
+
+const BASE_URL_ENV = import.meta.env.VITE_STATUS_INFO_API_BASE_URL;
+
+export const STATUS_INFO_API_BASE_URL = BASE_URL_ENV
+  ? removeTrailingSlash(BASE_URL_ENV)
+  : 'http://0.0.0.0:5747';
